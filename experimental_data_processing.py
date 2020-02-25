170dc078029d62a423e48eaefcdb480bdb3d202f
Justin L Ross <37995778+justinlross@users.noreply.github.com>
2020-02-25 10:53:50 +1000

    Add files via upload

--- experimental_data_processing.py
+++ experimental_data_processing.py
@@ -27,13 +27,14 @@ import cv2 # For the video file
 # Declare variables and constants
 
 
-asciiFolder = 'C:\\Users\Justin\Desktop\Datasets\Heralded Diffraction SM\*.asc'
-pngFolder = 'C:\\Users\Justin\Desktop\Datasets\Schrodinger Equation vs Real Experimental Data\images\*.png'
+asciiFolder = 'C:\\Users\Justin\Desktop\Datasets\Ghost Imaging SM\*.asc'
+pngFolder = 'C:\\Users\Justin\Desktop\Datasets\Ghost Imaging SM Contents\images\*.png'
+csvFolder = 'C:\\Users\Justin\Desktop\Datasets\Schrodinger Equation vs Real Experimental Data\csvheraldedSM\*.csv'
 videoFileName = 'videoaaazzzz.avi'
 
 
 ######################################################################
-
+# Functions and Code
 
 
 def convert_ascii_to_png(asciiFolder):
@@ -119,7 +120,7 @@ def convert_ascii_to_csv(asciiFolder):
         
     
         #This is for saving the file.
-        saveName ='csva/' + (str("%04d" % counter) + '.csv')
+        saveName ='csvghost/' + (str("%04d" % counter) + '.csv')
         dfend.to_csv(saveName)
 
         #This is for clearning the stored data.
@@ -135,16 +136,22 @@ def convert_ascii_to_csv(asciiFolder):
         counter += 1
     return
 
+
+"""
 def convert_csv_to_png():
     lines = []
     collisionMatrix = np.array([])
     counter = 0
-    list_of_files = glob.glob(pngFolder)
+    list_of_files = glob.glob(csvFolder)
     
     for filePath in list_of_files:
-        with open(filePath,encoding="csv") as dataInput:
+        # with open(filePath,encoding="csv") as dataInput:
+        # with open(filePath, newline='') as dataInput: # BLAH BLAH
+        with open(filePath, header=None) as dataInput: # BLAH BLAH
             for line in dataInput:
+                #print(line)
                 line = line.split()
+                print(line)
                 for item in line:
                     item = item.strip()
                     if item.isnumeric():
@@ -155,6 +162,52 @@ def convert_csv_to_png():
         grid = np.swapaxes(grid,0,1) # check to make sure this is the correct axis swap, probably a better way to do this.
 
         dfend = pd.DataFrame(grid)
+    
+        # Style the plot
+        fig, ax = plt.subplots()
+        heatmap = ax.pcolor(dfend, cmap=plt.cm.viridis, vmin=0, vmax=10)
+        
+        # The following is for getting rid of the white border.
+        ax.get_xaxis().set_ticks([])
+        ax.get_yaxis().set_ticks([])
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        ax.spines['bottom'].set_visible(False)
+        ax.spines['left'].set_visible(False)
+
+        plt.box(on=None)
+        plt.axis('off')
+
+        #Viridis is chosen as it is a great colour map that is dark to light.
+        fig.set_size_inches(14, 14)
+    
+    
+        #This is for saving the file.
+        saveName ='imagescsvheralded' + (str("%04d" % counter) + '.png')
+        #plt.savefig(saveName,bbox_inches='tight',pad_inches = -1)
+        #plt.savefig(saveName)
+        plt.savefig(saveName,bbox_inches='tight',aspect='auto', pad_inches = -0)
+        plt.close('all')
+
+        #This is for clearning the stored data.
+        collisionMatrix = np.array([]) # This is the line causing the issue, unclear why.
+        dfend = np.array([])
+        grid = np.array([])
+        lines.clear()
+        counter += 1
+    return
+"""
+
+def convert_csv_to_png():
+    lines = []
+    collisionMatrix = np.array([])
+    counter = 0
+    list_of_files = glob.glob(csvFolder)
+    
+    for filePath in list_of_files:
+        # with open(filePath,encoding="csv") as dataInput:
+        # with open(filePath, newline='') as dataInput: # BLAH BLAH
+        dfend = pd.read_csv(filePath, header=None) # BLAH BLAH
     
         """
         Section for styling the plot.
@@ -179,7 +232,7 @@ def convert_csv_to_png():
     
     
         #This is for saving the file.
-        saveName ='imagescsv' + (str("%04d" % counter) + '.png')
+        saveName ='imagescsvheralded' + (str("%04d" % counter) + '.png')
         #plt.savefig(saveName,bbox_inches='tight',pad_inches = -1)
         #plt.savefig(saveName)
         plt.savefig(saveName,bbox_inches='tight',aspect='auto', pad_inches = -0)
@@ -200,7 +253,7 @@ def ascii_to_png_display():
     rowVar = pd.Series()
     df = np.array([])
 
-    filePath = r"C:\\Users\Justin\Desktop\Datasets\Heralded Diffraction SM\3999.asc"
+    filePath = r"C:\\Users\Justin\Desktop\Datasets\Ghost Imaging SM\4070.asc"
 
 
     with open(filePath,encoding="ascii") as dataInput:
@@ -214,13 +267,13 @@ def ascii_to_png_display():
     df = np.append(df, [lines]) # perhaps use vertical stack here!!!
     grid = np.resize(df,(512, 512)) # 262144 produces 512 by 512
     grid = np.swapaxes(grid,0,1) # check to make sure this is the correct axis swap, probably a better way to do this.
-
+    print(np.max(grid))
     dfend = pd.DataFrame(grid)
 
     # styling
     # Style the plot
     fig, ax = plt.subplots()
-    heatmap = ax.pcolor(dfend, cmap=plt.cm.viridis, vmin=0, vmax=10)
+    heatmap = ax.pcolor(dfend, cmap=plt.cm.viridis, vmin=0, vmax=78)
     #Viridis is chosen as it is a great colour map that is dark to light.
     fig.set_size_inches(14, 14)
 
@@ -266,6 +319,6 @@ Uncomment whatever function you need to run.
 
 #convert_ascii_to_png(asciiFolder)
 #convert_ascii_to_csv(asciiFolder)
-#convert_csv_to_png()
+convert_csv_to_png()
 #create_video_from_png_files(pngFolder,videoFileName)
 #ascii_to_png_display()
